346e2392dea9f1e4a4b9ed019fdd958527be500f
marko-miletic <marko.g.miletic@gmail.com>
2022-11-03 11:23:44 +0100

    refactor: database creation refactoring

---
+++ src/core/config.py
@@ -0,0 +1,9 @@
+from dotenv import dotenv_values
+
+def get_db_connection_url() -> str:
+    config = dotenv_values(".env")
+    db_connection_url = (
+        f"postgresql+psycopg2://{config['POSTGRES_DB']}:{config['POSTGRES_PW']}@"
+        f"{config['POSTGRES_HOST']}:{config['POSTGRES_PORT']}/postgres"
+    )
+    return db_connection_url
--- src/models/results.py
+++ src/models/results.py
@@ -3,14 +3,17 @@ from base import BaseClass
 
 
 class RaceResults(BaseClass):
+    
     _id = Column(Integer, primary_key=True, index=True)
-    _track_id = Column(Integer, ForeignKey("tracks._id"))
+
     position = Column(Integer, nullable=False, unique=False)
     no = Column(Integer, nullable=False, unique=False)
-    _driver_id = Column(Integer, ForeignKey("drivers._id"))
-    _team_id = Column(Integer, ForeignKey("teams._id"))
     starting_grid = Column(Integer, nullable=False, unique=False)
     laps = Column(Integer, nullable=False, unique=False)
     total_time_gap_retirement = Column(String(50), nullable=False, unique=False)
     points = Column(Integer, nullable=False, unique=False)
     fastest_lap = Column(String(50), nullable=False, unique=False)
+
+    _track_id = Column(Integer, ForeignKey("tracks._id"))
+    _driver_id = Column(Integer, ForeignKey("drivers._id"))
+    _team_id = Column(Integer, ForeignKey("teams._id"))
